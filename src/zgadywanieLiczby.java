946729ecdd587ac09c7f4a433a7aac8ebbac2503
Konsztel <pawcio574@interia.pl>
2025-04-27 15:38:38 +0200

    Update to 2.0

--- src/zgadywanieLiczby.java
+++ src/zgadywanieLiczby.java
@@ -1,30 +1,48 @@
+//Version 2.0
+
 import java.util.Random;
 import java.util.Scanner;
 
-public class zgadywanieLiczby {
-    public void main() {
-    }
 
-    public static void main(String[] args) {
-        int licznik = 1;
-        Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
-        int n = rand.nextInt(51);
-        System.out.println("Zgadnij o jakiej liczbie myśle od 0 do 50");
-        int a = scan.nextInt();
+public static void main(String[] args) {
+    int i = 1;
+    Scanner scan = new Scanner(System.in);
+    Random rand = new Random();
+    int n = rand.nextInt(51);
 
-        while(a != n) {
-            if (a > n) {
-                System.out.println("Podałeś za dużą liczbe, spróbuj jeszcze raz");
-                a = scan.nextInt();
-                ++licznik;
-            } else if (a < n) {
-                System.out.println("Podałeś za małą liczbe, spróbuj jeszcze raz");
-                a = scan.nextInt();
-                ++licznik;
-            }
+    System.out.println("Zgadnij o jakiej liczbie myśle od 0 do 50");
+    int a = scan.nextInt();
+
+    while(a != n) {
+        if (a > n) {
+            System.out.println("Podałeś za dużą liczbe, spróbuj jeszcze raz");
+        } else if (a < n) {
+            System.out.println("Podałeś za małą liczbe, spróbuj jeszcze raz");
         }
 
-        System.out.println("Brawo zgadłeś o jakiej liczbie myślałem za " + licznik + " razem");
+        i++;
+        licznikPrób(i);
+        a = scan.nextInt();
+        pozostałePróby(i);
+    }
+    System.out.println("Brawo zgadłeś o jakiej liczbie myślałem za " + i + " razem");
+}
+
+public static void licznikPrób(int i) {
+    if (i > 10) {
+        System.out.println("Przekroczyłeś 10 prób! Gra zakończona.");
+        System.exit(0);
     }
 }
+
+public static void pozostałePróby(int i){
+    int x = 10 - i;
+    System.out.println("Pozostało " + x + " Prób");
+}
+
+
+//Na koniec po zgadnięciu pytać czy chcesz zagrać ponownie
+//Zapytać też po przekroczeniue 10 prób czy chce zagrać ponownie
+
+//Sprawdzanie czy wprowadzono poprawne dane
+
